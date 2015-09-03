1fe3caaf8239eb88a0882732929c587a4a442c37
Nate Palmer <netpalm555@gmail.com>
2015-09-03 10:50:24 -0400

    Merged materialize test site.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
@@ -10,13 +9,9 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+app.get('/', express.static("public"));
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
+app.get('/material', express.static("public"));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
