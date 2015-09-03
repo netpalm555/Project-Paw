2faeee3a2ed9a4ac79d9a79163d2cb655b3092e3
Nate Palmer <netpalm555@gmail.com>
2015-09-03 12:30:34 -0400

    Just updating

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
