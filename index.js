9c1bb6a3559bfec12c040f3817ffc60975a1ac36
Nate Palmer <netpalm555@gmail.com>
2015-09-03 12:04:52 -0400

    Now even better.

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-app.use("/res", express.static(__dirname + '/res'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
