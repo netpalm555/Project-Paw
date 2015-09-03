62ad10bbad517b534c20663a8a7e4e4794cb0f5b
Nate Palmer <netpalm555@gmail.com>
2015-09-03 11:49:56 -0400

    Still optimizing code.

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use("/res", express.static(__dirname + '/res'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', express.static("public"));
-
-app.get('/material', express.static("public"));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
