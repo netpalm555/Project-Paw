d73c4229b2af25c9a7057e148939d389f6a202e7
Nate Palmer <netpalm555@gmail.com>
2015-09-04 08:23:09 -0400

    Small changes (nothing important).

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 var express = require('express');
+var pg = require('pg');
 var app = express();
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 app.set('port', (process.env.PORT || 5000));
 
