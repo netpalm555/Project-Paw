75514f26d05a0b8d59ced7f6fbff94d677976bf7
Nathan Palmer <netpalm555@gmail.com>
2015-09-05 10:44:31 -0400

    First steps.

--- index.js
+++ index.js
@@ -2,15 +2,27 @@ var express = require('express');
 var pg = require('pg');
 var app = express();
 
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
+var pgURL = {
+  user: "dahrttupatsgrc",
+  password: "JphS59a9GcRUHnhuHkTbHjvzFu",
+  database: "d9hupdecqpkcup",
+  port: 5432,
+  host: "ec2-107-21-106-196.compute-1.amazonaws.com",
+  ssl: true
+};
+
+pg.connect(pgURL, function(err, client) {
+  if (err) {
+    console.log('Error getting ' + pgURL + " " + err);
+    throw err;
+  }
   console.log('Connected to postgres! Getting schemas...');
 
-  client
+  /*client
     .query('SELECT table_schema,table_name FROM information_schema.tables;')
     .on('row', function(row) {
       console.log(JSON.stringify(row));
-    });
+    });*/
 });
 
 app.set('port', (process.env.PORT || 5000));
