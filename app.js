0966dba64339909969f00f4d703b719d93690e7f
Nate Palmer <netpalm555@gmail.com>
2015-10-16 00:22:10 -0400

    Hopefully this works

--- app.js
+++ app.js
@@ -33,16 +33,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var dbUrl = process.env.DATABASE_URL || "postgres://dahrttupatsgrc:JphS59a9GcRUHnhuHkTbHjvzFu@ec2-107-21-106-196.compute-1.amazonaws.com:5432/d9hupdecqpkcup?ssl=true";
 
-pg.connect(dbUrl, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT * FROM public.testing')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
+// pg.connect(dbUrl, function(err, client) {
+//   if (err) throw err;
+//   console.log('Connected to postgres! Getting schemas...');
+//
+//   client
+//     .query('SELECT * FROM public.testing')
+//     .on('row', function(row) {
+//       console.log(JSON.stringify(row));
+//     });
+// });
 
 app.use('/', routes);
 
