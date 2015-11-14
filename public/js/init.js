53379d8a24cf0829afca9cadd788e036e4b2a706
Nate Palmer <netpalm555@gmail.com>
2015-11-13 20:03:42 -0500

    Fixed the issue with parallax and it works now.

--- public/js/init.js
+++ public/js/init.js
@@ -1,7 +1,7 @@
 (function($){
   $(function(){
-
-    // $('.button-collapse').sideNav();
-
+    $(document).ready(function(){
+      $('.parallax').parallax();
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
