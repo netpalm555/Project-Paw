0e8a44590f421ccb64214d74fd53ff4333ad2bab
Nate Palmer <netpalm555@gmail.com>
2015-11-17 10:50:07 -0500

    Starting work on angular.

--- public/app/directives.js
+++
@@ -1,7 +0,0 @@
-var directives = angular.module('myapp.directives', []);
-directives.directive('hello', function () {
-    return {
-        restrict: 'E',
-        template: '<p>Hello from directive</p>'
-    };
-});
