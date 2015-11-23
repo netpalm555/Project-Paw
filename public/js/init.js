89bc4d26679e5007b56b25907ce5415fd0012aab
Nate Palmer <netpalm555@gmail.com>
2015-11-23 11:02:20 -0500

    Fixed extraneous jQuery

--- public/js/init.js
+++ public/js/init.js
@@ -1,8 +1,6 @@
-(function($){
-  $(function(){
-    $(document).ready(function(){
-      $('.parallax').parallax();
-      $(".dropdown-button").dropdown();
-    });
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+$(function() {
+  $(document).ready(function() {
+    $('.parallax').parallax();
+    $(".dropdown-button").dropdown();
+  });
+}); // end of document ready
